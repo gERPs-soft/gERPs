d7f9a296a1ced6ea66462536874dd2b8dba92689
Stanisław Szypowski <sszypows@gmail.com>
2018-11-20 21:47:03 +0100

    Addded http service do postOrder to Order module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpService} from './http.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gERPs';
+
+  itema: OrderItem = new OrderItem(1, 2);
+  itemb: OrderItem = new OrderItem(2, 4);
+  itemList: Array<OrderItem> = [this.itema, this.itemb];
+  order: Order = new Order(1, 1, this.itemList);
+
+  constructor(private httpService: HttpService) {
+  }
+
+  sendOrder() {
+    this.httpService.postOrder(this.order).subscribe(status => console.log(status));
+  }
+}
+
+export class Order {
+  customerId: number;
+  sellerId: number;
+  OrderItemList: Array<OrderItem>;
+
+  constructor(customerId: number, sellerId: number, OrderItemList: Array<OrderItem>) {
+    this.customerId = customerId;
+    this.sellerId = sellerId;
+    this.OrderItemList = OrderItemList;
+  }
+}
+
+export class OrderItem {
+  private productId: number;
+  private quantity: number;
+
+
+  constructor(productId: number, quantity: number) {
+    this.productId = productId;
+    this.quantity = quantity;
+  }
+}
+
+export class OrderStatus {
+  private deliveryTime: string;
+
+  constructor(deliveryTime: string) {
+    this.deliveryTime = deliveryTime;
+  }
 }
