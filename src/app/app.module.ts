b71c4edfeb3a67786bc3d3935d648696d83c8162
Stanisław Szypowski <sszypows@gmail.com>
2018-11-21 21:06:55 +0100

    Http service moved to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpService} from './http.service';
+import {HttpService} from './services/http.service';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
