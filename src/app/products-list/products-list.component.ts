a7204966ae7217b636dfa0d51ccfb7817702b66e
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-11-24 17:38:00 +0100

    Add components: product-list and products

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from '../products/products.component';
+import {HttpService} from '../services/http.service';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+
+  products: Array<Product>;
+
+  constructor(private http: HttpClient, private httpService: HttpService) {
+  }
+
+  ngOnInit() {
+    this.httpService.getAllProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../services/http.service';
+import {SetterFn} from '@angular/core/src/reflection/types';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  product: Product = new Product(null,
+    '1.07.003', 'Papier STANDARD 19 1w szary', 1, 'szt', '1111004411',
+4, 'Folia', 12, 20, 40, 60, 2, 1400, 'VAT_23');
+
+  constructor(private httpService: HttpService) { }
+
+  ngOnInit() {
+  }
+
+  addProduct() {
+    this.httpService.postAddProduct(this.product).subscribe(status => console.log(status));
+  }
+}
+
+export class Product {
+  id: number;
+  assort_index: string;
+  name: string;
+  product_group: number;
+  unitOfMasure: string;
+  barcode: string;
+  weight_unit: number;
+  package_unit: string;
+  number_in_package: number;
+  height: number;
+  weight: number;
+  length: number;
+  supplier: number;
+  stock: number;
+  vat: string;
+
+  constructor(id: number, assort_index: string, name: string, product_group: number, unitOfMasure: string,
+              barcode: string, weight_unit: number, package_unit: string, number_in_package: number, height: number,
+              weight: number, length: number, supplier: number, stock: number, vat: string) {
+    this.id = id;
+    this.assort_index = assort_index;
+    this.name = name;
+    this.product_group = product_group;
+    this.unitOfMasure = unitOfMasure;
+    this.barcode = barcode;
+    this.weight_unit = weight_unit;
+    this.package_unit = package_unit;
+    this.number_in_package = number_in_package;
+    this.height = height;
+    this.weight = weight;
+    this.length = length;
+    this.supplier = supplier;
+    this.stock = stock;
+    this.vat = vat;
+  }
+}
