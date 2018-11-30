56b59863cb603ef811e12e05f1f1a7bdad27ad18
Stanisław Szypowski <sszypows@gmail.com>
2018-11-30 15:20:05 +0100

    Added customer view

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from '../services/http.service';
+import {Order} from '../order/order.component';
 
 @Component({
   selector: 'app-customer',
@@ -8,16 +9,25 @@ import {HttpService} from '../services/http.service';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() {
+  customers: Array<Customer>;
+
+  constructor(private httpService: HttpService) {
   }
 
   ngOnInit() {
+  }
+
+  getCustomers() {
+    this.httpService.getAllCustomers().subscribe(data => {
+      this.customers = data.slice();
+    });
   }
 
 }
 
 export class Customer {
 
+  private id: number;
   private firstName: string;
   private lastName: string;
   private companyName: string;
@@ -34,4 +44,8 @@ export class Customer {
 
 enum CustomerType {
   BUSINESS, INDIVIDUAL
+}
+
+export class CustomerStatus {
+  private status: string;
 }
