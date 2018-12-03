64c6890cd99975be19e2ac3b4797882ee74575fb
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-12-04 00:47:10 +0100

    Big refactor products-components

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,15 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {OrderStatus} from '../order/order.component';
-import {Product} from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
-  private magazineUrl = 'http://localhost:8080';
 
   constructor() {
   }
