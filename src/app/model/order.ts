d14a32595278dbd3dd69863296b0cf7cb79a0526
Stanisław Szypowski <sszypows@gmail.com>
2018-12-08 00:38:35 +0100

    Order component updated - still problem with Array<OrderItem>

--- src/app/model/order.ts
+++ src/app/model/order.ts
@@ -9,14 +9,7 @@ export class Order {
   orderDate: string;
   orderStatus: string;
 
-  constructor(orderId: number, sellerId: number, customerId: number, orderItemsList: Array<OrderItem>,
-              endDate: string, orderDate: string, orderStatus: string) {
-    this.orderId = orderId;
-    this.sellerId = sellerId;
-    this.customerId = customerId;
-    this.orderItemsList = orderItemsList;
-    this.endDate = endDate;
-    this.orderDate = orderDate;
-    this.orderStatus = orderStatus;
+
+  constructor() {
   }
 }
--- src/app/order-edit/order-edit.component.ts
+++ src/app/order-edit/order-edit.component.ts
@@ -22,7 +22,7 @@ export class OrderEditComponent implements OnInit {
   customerList = new Array<Customer>();
   private orderItemsList: OrderItem[];
   orderForm: FormGroup;
-  newOrder: Order;
+  newOrder = new Order();
   @Input()
   initOrder: Order;
 
@@ -34,8 +34,7 @@ export class OrderEditComponent implements OnInit {
     this.productsService.getAllProducts().subscribe(data => this.productList = data.slice());
     this.customerService.getAllCustomers().subscribe(data => this.customerList = data.slice());
     this.orderForm = new FormGroup({
-      customer: new FormControl(),
-      itemList: new FormArray([])
+      customer: new FormControl()
     });
   }
 
@@ -51,8 +50,6 @@ export class OrderEditComponent implements OnInit {
 
   chooseItem(item: Item) {
     if (item.quantity > 0 && !this.chosenProducts.includes(item)) {
-      const arr = <FormArray>this.orderForm.get('itemList');
-      arr.push(new FormControl(null));
       this.chosenProducts.push(item);
     }
   }
@@ -71,10 +68,6 @@ export class OrderEditComponent implements OnInit {
     item.quantity++;
   }
 
-  get items(): FormArray {
-    return this.orderForm.get('itemList') as FormArray;
-  }
-
   getOrderItems(items: Array<Item>): Array<OrderItem> {
     for (const item of items) {
       this.orderItemsList.push(new OrderItem(item.product.id, item.quantity, item.product.price));
@@ -83,8 +76,8 @@ export class OrderEditComponent implements OnInit {
   }
 
   onSubmit() {
-    this.newOrder.customerId = this.orderForm.get('customer').get('id').value;
-    this.newOrder.orderItemsList = this.getOrderItems(this.items.value);
+    this.newOrder.customerId = this.orderForm.value.customer;
+    this.newOrder.orderItemsList = this.getOrderItems(this.chosenProducts);
     this.newOrder.sellerId = 1;
     this.orderService.postOrder(this.newOrder).subscribe(() => this.eventForm.emit(true));
   }
